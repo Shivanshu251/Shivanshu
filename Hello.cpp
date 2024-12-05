53b985ac0f918f1d6e00d0fdfbc02a98823f9a26
Shivanshu251 <shivanshutripathi2505@gmail.com>
2024-12-05 22:07:26 +0530

    565657

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a No.";
+    cin>>n;
+    if(n%2==0)
+    cout<<"Given No. is Even";
+    else
+    cout<<"Given no. is odd";
+    return 0;
+}
